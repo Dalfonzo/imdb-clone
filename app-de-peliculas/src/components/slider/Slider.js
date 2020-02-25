52be5beb71ac4310878477ac831b2ec5dc597f36
dap <dalfonzo18@gmail.com>
2020-02-25 19:06:21 -0400

    slider terminado , falta cambiarle unos detalles para hacerlo más dinámico

--- app-de-peliculas/src/components/slider/Slider.js
+++ app-de-peliculas/src/components/slider/Slider.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './Slider.module.scss';
 import Card from './slider-card/Card';
 import axios from 'axios';
+import Controls from '../carousel/carousel-controls/Controls';
 
 class Slider extends React.Component {
   state = {
@@ -19,28 +20,49 @@ class Slider extends React.Component {
     });
   };
 
+  adelante = () => {
+    this.setState(prvState => ({
+      position: prvState.position - 100
+    }));
+  };
+
+  atras = () => {
+    this.setState(prvState => ({
+      position: prvState.position + 100
+    }));
+  };
+
   componentDidMount() {
     this.getMovies();
   }
 
   render() {
     const { data, position } = this.state;
-    
+    const translateSlider = { transform: `translateX(${position}%)` };
+    const disableForward = Math.abs(position / 100 ) === Math.floor(data.length / 6)
+    const disableBack = !position
+
+    console.log(disableForward)
     return (
       <>
-        <button onClick={this.getMovies}>clickme</button>
         <div className={classes.wrapper}>
           <h2 className={classes.title}>What to Watch</h2>
           <h3 className={classes.subtitle}>Fan Favorites</h3>
           <p className={classes.description}>This week's top TV and movies</p>
           <div className={classes.container1}>
-            <div className={classes.container2}></div>
+            <div className={classes.container2} style={translateSlider}>
+              {data.map(movie => {
+                return <Card movie={movie} />;
+              })}
+            </div>
+            <Controls
+              disableBack={disableBack}
+              disableForward={disableForward}
+              back={this.atras}
+              forward={this.adelante}
+            />
           </div>
         </div>
-        {data.map(movie => {
-          console.log(movie)
-          return <Card movie={movie} />;
-        })}
       </>
     );
   }
--- app-de-peliculas/src/components/slider/slider-card/Card.js
+++ app-de-peliculas/src/components/slider/slider-card/Card.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import classes from './Card.module.scss'
+import classes from './Card.module.scss';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faStar, faPlus, faBookmark } from '@fortawesome/free-solid-svg-icons';
 
-const card = ({movie}) => {
-  const baseUrl = 'https://image.tmdb.org/t/p/w500'
+const card = ({ movie }) => {
+  const baseUrl = 'https://image.tmdb.org/t/p/w185';
   return (
-    
+    <div className={classes.wrapper}>
       <div className={classes.container}>
-        <p>{movie.title}</p>
-        <p>{movie.vote_average}</p>
-        <img src={baseUrl + movie.poster_path} alt=""/>
+        <Icon icon={faBookmark} className={classes.bookmark} />
+        <Icon icon={faPlus} className={classes.plus} />
+
+        <img src={baseUrl + movie.poster_path} alt="" />
+        <p className={classes.vote}>
+          <Icon icon={faStar} className={classes.icon} />
+          {movie.vote_average.toFixed(1)}
+        </p>
+        <p className={classes.title}>{movie.title}</p>
+        <p className={classes.button}>
+          <Icon icon={faPlus} className={classes.icon} />
+          Watchlist
+        </p>
       </div>
-    
+    </div>
   );
 };
 
