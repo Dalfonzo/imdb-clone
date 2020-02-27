db279bdb5f8f3c765c7282037f11d1b39a6bc1bb
dap <dalfonzo18@gmail.com>
2020-02-27 10:54:31 -0400

    organizado en paginas para agregar las demás secciones

---
+++ app-de-peliculas/src/pages/home/Home.js
@@ -0,0 +1,70 @@
+import React from 'react'; 
+import Carousel from '../../components/carousel/Carousel';
+import Slider from '../../components/movies-slider/Slider';
+import News from '../../components/news-slider/News';
+import Celebrities from '../../components/celebrities-slider/Celebrities';
+
+import classes from './Home.module.scss';
+
+const getToday = () => {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+  let today = new Date();
+  let day = String(today.getDate()).padStart(2, '0');
+  let month = String(months[today.getMonth()]);
+  today = `${month} ${day}`;
+
+  return today;
+};
+
+const home = () => {
+  const today = getToday();
+
+  return (
+    <div className={classes.container}>
+      <Carousel url="https://api.themoviedb.org/3/movie/upcoming?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=1" />
+      <Slider
+        title="What to Watch"
+        subtitle="Fan Favorites"
+        description="This week's top TV and movies"
+        url="https://api.themoviedb.org/3/movie/popular?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=1"
+      />
+      <News
+        title="More to explore"
+        subtitle="Top news"
+        url="http://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=c89fb28b98ed4170a4004bab8835654b"
+      />
+      <Slider
+        title="Explore what’s streaming"
+        subtitle="Prime Video"
+        description="Included with Prime"
+        url="https://api.themoviedb.org/3/movie/popular?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=2"
+      />
+      <Slider
+        title="Explore movies & TV shows"
+        subtitle="In theathers"
+        description="Showtimes near you"
+        url="https://api.themoviedb.org/3/movie/now_playing?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=1"
+      />
+      <Celebrities
+        subtitle="Born Today"
+        description={`People born on ${today}`}
+        url="https://api.themoviedb.org/3/person/popular?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=1"
+      />
+    </div>
+  );
+};
+
+export default home;
