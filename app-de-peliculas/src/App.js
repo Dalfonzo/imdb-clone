ba864ae4ad5f27c693bbd3048149e4e65ee90393
dap <dalfonzo18@gmail.com>
2020-02-27 11:01:18 -0400

    agregado Switch y Route

--- app-de-peliculas/src/App.js
+++ app-de-peliculas/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Header from './components/header/Header';
-import Home from './pages/home/Home'
+import Home from './pages/home/Home';
 import Footer from './components/footer/Footer';
 
 import './App.css';
@@ -15,7 +16,11 @@ class App extends Component {
     return (
       <>
         <Header />
-        <Home/>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
         <Footer />
       </>
     );
