80f483a86a587e817bc7731783af40efe31e9044
dap <dalfonzo18@gmail.com>
2020-02-27 10:42:52 -0400

    cambio de layout

--- app-de-peliculas/src/App.js
+++ app-de-peliculas/src/App.js
@@ -40,41 +40,41 @@ class App extends Component {
   render() {
     const today = this.getToday();
     return (
-      <div className="contenedor">
+      <>
         <Header />
-        <Carousel
-          url="https://api.themoviedb.org/3/movie/upcoming?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=1"
-        />
-        <Slider
-          title="What to Watch"
-          subtitle="Fan Favorites"
-          description="This week's top TV and movies"
-          url="https://api.themoviedb.org/3/movie/popular?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=1"
-        />
-        <News
-          title="More to explore"
-          subtitle="Top news"
-          url="http://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=c89fb28b98ed4170a4004bab8835654b"
-        />
-        <Slider
-          title="Explore what’s streaming"
-          subtitle="Prime Video"
-          description="Included with Prime"
-          url="https://api.themoviedb.org/3/movie/popular?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=2"
-        />
-        <Slider
-          title="Explore movies & TV shows"
-          subtitle="In theathers"
-          description="Showtimes near you"
-          url="https://api.themoviedb.org/3/movie/now_playing?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=1"
-        />
-        <Celebrities
-          subtitle="Born Today"
-          description={`People born on ${today}`}
-          url="https://api.themoviedb.org/3/person/popular?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=1"
-        />
+        <div className="contenedor">
+          <Carousel url="https://api.themoviedb.org/3/movie/upcoming?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=1" />
+          <Slider
+            title="What to Watch"
+            subtitle="Fan Favorites"
+            description="This week's top TV and movies"
+            url="https://api.themoviedb.org/3/movie/popular?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=1"
+          />
+          <News
+            title="More to explore"
+            subtitle="Top news"
+            url="http://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=c89fb28b98ed4170a4004bab8835654b"
+          />
+          <Slider
+            title="Explore what’s streaming"
+            subtitle="Prime Video"
+            description="Included with Prime"
+            url="https://api.themoviedb.org/3/movie/popular?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=2"
+          />
+          <Slider
+            title="Explore movies & TV shows"
+            subtitle="In theathers"
+            description="Showtimes near you"
+            url="https://api.themoviedb.org/3/movie/now_playing?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=1"
+          />
+          <Celebrities
+            subtitle="Born Today"
+            description={`People born on ${today}`}
+            url="https://api.themoviedb.org/3/person/popular?api_key=3e2cc31e8a094dc74d7fa8c446b0c3fa&language=en-US&page=1"
+          />
+        </div>
         <Footer />
-      </div>
+      </>
     );
   }
 }
