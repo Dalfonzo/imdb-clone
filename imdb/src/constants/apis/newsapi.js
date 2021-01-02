b2778f7145590e02a8f828db7b9d5379231cb371
dap <dalfonzo18@gmail.com>
2021-01-02 16:15:38 -0400

    deploy

--- imdb/src/constants/apis/newsapi.js
+++ imdb/src/constants/apis/newsapi.js
@@ -2,4 +2,4 @@ const API_KEY = 'apiKey=c89fb28b98ed4170a4004bab8835654b';
 const BASE_URL = 'http://newsapi.org/v2/';
 const CONFIG = 'top-headlines?country=us&category=entertainment&';
 
-export const HEADLINES = BASE_URL + CONFIG + API_KEY;
+export const HEADLINES = BASE_URL + CONFIG + API_KEY + ':splat 200!';
